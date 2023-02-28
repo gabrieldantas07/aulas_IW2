a7902ac0fa3d1c7944f4e07ce6940066ab3b7be2
Gabriel Dantas <100931688+gabrieldantas07@users.noreply.github.com>
2023-02-28 12:34:49 -0300

    Troca de Páginas

---
+++ pasta/js/code.js
@@ -0,0 +1,24 @@
+function backhome(){
+    window.location.href = "index.html"
+}
+
+function page2(){
+    window.location.href = "second.html"
+}
+
+function page3(){
+    window.location.href = "third.html"
+}
+
+function page4(){
+    window.location.href = "fourth.html"
+}
+
+function page5(){
+    window.location.href = "fifth.html"
+}
+
+
+
+
+
