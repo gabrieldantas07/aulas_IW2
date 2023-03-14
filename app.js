f68e8c183d8df38e33623359c9e72b4d33f0919f
Gabriel Dantas <100931688+gabrieldantas07@users.noreply.github.com>
2023-03-14 12:26:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+
+app.get("/", function(req,res){
+    res.send("Minha primeira página")
+})
+
+app.get("/sobre", function(req,res){
+    res.send("Minha página sobre")
+})
+
+app.get("/historia", function(req,res){
+    res.send("Minha página de história")
+})
+
+app.get("/caneca", function(req,res){
+    res.sendFile(__dirname + "/html/caneca.html")
+})
+
+app.get("/copo", function(req,res){
+    res.sendFile(__dirname + "/html/copo.html")
+})
+
+app.listen(8081, function(req,res){
+    console.log("Servidor Funcionando!")
+})
+
